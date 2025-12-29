0ae800062d643a610e9ba86bc6ff4fd42dec464f
yolaidi <youneslaydi7@gmai.com>
2025-12-29 16:43:17 +0100

    add readme

--- backend/src/main/java/com/_Blog/backend/controller/Controller.java
+++ backend/src/main/java/com/_Blog/backend/controller/Controller.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @GetMapping("/")
+    public String Hello() {
+        return "Hello, Spring Boot!";
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, Spring Boot!";
