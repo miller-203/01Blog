2574e68ab5c2575516f5ebf154b1d50b71766a78
yolaidi <youneslaydi7@gmai.com>
2025-12-29 18:43:01 +0100

    first day

---
+++ backend/src/main/java/com/_Blog/backend/model/User.java
@@ -0,0 +1,29 @@
+package com._Blog.backend.model; // <--- CHANGED THIS LINE
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role; 
+
+    private String profilePicUrl;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
