eb17a2eff15f5b6cd88f372354d11755fe4f6500
miller-203 <youneslaydi7@gmail.com>
2026-04-04 14:31:23 +0100

    push

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
-import { Welcome } from './features/dashboard/welcome/welcome';
 import { AuthLayout } from './layouts/auth-layout/auth-layout';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { Home } from './features/dashboard/home/home';
@@ -11,8 +10,6 @@ import { AdminUsers } from './features/admin/admin-users/admin-users';
 import { AdminPosts } from './features/admin/admin-posts/admin-posts';
 import { AdminReports } from './features/admin/admin-reports/admin-reports';
 import { SavedPosts } from './features/saved-posts/saved-posts';
-import { Privacy } from './features/privacy/privacy';
-import { About } from './features/about/about';
 import { Settings } from './features/settings/settings';
 import { Notifications } from './features/notifications/notifications';
 import { authGuard } from './core/guard/auth-guard';
@@ -28,9 +25,9 @@ export const routes: Routes = [
     component: AuthLayout,
     canActivate: [noAuthGuard],
     children: [
-      { path: '', component: Welcome },
-      { path: 'register', component: Register },
+      { path: '', component: Login },
       { path: 'login', component: Login },
+      { path: 'register', component: Register },
     ]
   },
   {
@@ -41,8 +38,6 @@ export const routes: Routes = [
       { path: 'home', component: Home },
       { path: 'home/fallowd-posts', component: Home },
       { path: 'saved-posts', component: SavedPosts },
-      { path: 'privacy', component: Privacy },
-      { path: 'about', component: About },
       { path: 'settings', component: Settings },
       { path: 'notifications', component: Notifications },
       { path: 'create-post', component: CreatePost },
