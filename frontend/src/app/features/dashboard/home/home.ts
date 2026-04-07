fe5b77539a034c543e2c782432b7a1e9c2141c60
Miller <158384878+miller-203@users.noreply.github.com>
2026-04-07 15:28:16 +0100

    Adjust sidebar visibility between home and main layout

--- frontend/src/app/features/dashboard/home/home.ts
+++ frontend/src/app/features/dashboard/home/home.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, AfterViewInit, ViewChild, inject } from '@angular/core';
-import { SidebarLeft } from '../../../components/sidebar-left/sidebar-left';
 import { PostService } from '../../../core/services/post.service';
 import { PostCard } from '../../../components/post-card/post-card';
 import { parseEditorJsContent } from '../../../core/utils/editorjs-parser';
@@ -10,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FormsModule, SidebarLeft, PostCard, Popup],
+  imports: [CommonModule, FormsModule, PostCard, Popup],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
