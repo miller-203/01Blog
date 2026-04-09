de935e65836519636285e17cbcaf6e13c9f5af04
miller-203 <youneslaydi7@gmail.com>
2026-04-09 10:36:10 +0100

    push

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,7 +10,6 @@ import { AdminUsers } from './features/admin/admin-users/admin-users';
 import { AdminPosts } from './features/admin/admin-posts/admin-posts';
 import { AdminReports } from './features/admin/admin-reports/admin-reports';
 import { SavedPosts } from './features/saved-posts/saved-posts';
-import { Settings } from './features/settings/settings';
 import { Notifications } from './features/notifications/notifications';
 import { authGuard } from './core/guard/auth-guard';
 import { adminGuard } from './core/guard/admin-guard';
@@ -38,7 +37,6 @@ export const routes: Routes = [
       { path: 'home', component: Home },
       { path: 'home/fallowd-posts', component: Home },
       { path: 'saved-posts', component: SavedPosts },
-      { path: 'settings', component: Settings },
       { path: 'notifications', component: Notifications },
       { path: 'create-post', component: CreatePost },
       { path: 'edit-post/:id', component: CreatePost },
